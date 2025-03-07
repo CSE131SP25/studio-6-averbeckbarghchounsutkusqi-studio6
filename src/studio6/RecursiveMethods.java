47c0cff21d1b49eb40415da95f0ec162ed47bff8
Rayan Barghchoun <barghchounr@yahoo.com>
2025-03-06 20:10:26 -0600

    working array reversal

--- src/studio6/RecursiveMethods.java
+++ src/studio6/RecursiveMethods.java
@@ -14,8 +14,11 @@ public class RecursiveMethods {
 	public static double geometricSum(int n) {
 		
 			// FIXME compute the geometric sum for the first n terms recursively
-			return 0;
-		
+			if (n == 0) {
+				return 0;
+			} else {
+				return 1 / Math.pow(2,n) + geometricSum(n - 1);
+			}		
 	}
 
 	
@@ -30,7 +33,17 @@ public class RecursiveMethods {
 	 */
 	public static void circlesUponCircles(double xCenter, double yCenter, double radius, double radiusMinimumDrawingThreshold) {
 		
+		
 		// FIXME complete the recursive drawing
+		if (radius < radiusMinimumDrawingThreshold) {
+
+		} else {
+			StdDraw.circle(xCenter, yCenter, radius);
+			circlesUponCircles(xCenter, yCenter + radius, radius / 3, radiusMinimumDrawingThreshold);
+			circlesUponCircles(xCenter, yCenter - radius, radius / 3, radiusMinimumDrawingThreshold);
+			circlesUponCircles(xCenter + radius, yCenter, radius / 3, radiusMinimumDrawingThreshold);
+			circlesUponCircles(xCenter - radius, yCenter, radius / 3, radiusMinimumDrawingThreshold);
+		}
 	}
 	
 
@@ -41,9 +54,26 @@ public class RecursiveMethods {
 	 * @return an array with the same data as the input but it reverse order
 	 */
 	public static int[] toReversed(int[] array) {
-		
+			
+			int index = array.length - 1;
+			int[] arrayR = new int[array.length];
+
 			// FIXME create a helper method that can recursively reverse the given array
-			return new int[0];
+			return toReversedHelper(array, arrayR, index);
+
+		
+	}
+	
+	public static int[] toReversedHelper(int[] array, int[] newArray, int index) {
+		
+		if (index < 0) {
+			return newArray;
+		} else { 
+			
+			newArray[index] = array[(array.length - 1) - index];
+			
+			return toReversedHelper(array, newArray, index - 1);
+		}
 		
 	}
 
@@ -61,6 +91,5 @@ public class RecursiveMethods {
 			return 0;
 		
 	}
-
 
 }
